ce6123f176287f6370842df1911db2c349ccbb8a
Bartłomiej Długosz <dlugosz.bartlomiej@gmail.com>
2014-07-21 22:30:31 +0200

    proby ogarniecia

--- src/main/java/amg/net/pl/controler/HelloControler.java
+++ src/main/java/amg/net/pl/controler/HelloControler.java
@@ -3,11 +3,12 @@ package amg.net.pl.controler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HelloControler {
 	
-	@RequestMapping("/helloWorld")
+	@RequestMapping(value="/hello", method=RequestMethod.GET)
 	public String helloWorld(Model model) {
 		model.addAttribute("message", "Hello World!");
 		return "helloWorld";	
